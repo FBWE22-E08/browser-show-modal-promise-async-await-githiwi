9d4ab6e99cc608bef05635a22ac728e314250888
hiwi-dci-student <hiwot.abdi@dci.education>
2023-01-30 22:24:15 +0100

    bonus1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./css/style.css"; //importing css
+
+const box = document.querySelector(".modal");
+
+const myButton = document.getElementById("button");
+
+const boxPromise = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("block");
+    }, 3000);
+  });
+};
+
+const getResult = async () => {
+  let result = await boxPromise();
+  box.style.display = result;
+};
+myButton.addEventListener("click", myfunction);
+
+function myfunction() {
+  console.log("clicked");
+  box.style.display = "none";
+}
+
+getResult();
